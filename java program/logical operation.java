f09987b168b4acb338cce5d1b79ecfd120613fd2
Ankit Phuyal <ankitphuyal500@gmail.com>
2026-05-21 14:04:32 +0545

    Add files via upload

---
+++ java program/logical operation.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        boolean x = true;
+        boolean y = false;
+
+        System.out.println("\n logical operation");
+
+        // Logical AND
+        System.out.println("x && y = " + (x && y));
+
+        // Logical OR
+        System.out.println("x || y = " + (x || y));
+
+        // Logical NOT
+        System.out.println("!x = " + (!x));
+        System.out.println("!y = " + (!y));
+    }
+}
